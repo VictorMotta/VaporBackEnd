3060d87f2e71c7c4c8ed472c7296ebabe9dbe5d0
Victor Motta <victor.mottag@gmail.com>
2023-01-24 19:53:11 -0300

    Iniciado projeto

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const port = process.env.PORT || 5000;
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.listen(port, console.log(`Servidor iniciado com sucesso! Na porta: ${port}`));
