51d991d2f597dbb655245bbaca37d0a3f7b70504
Victor Motta <victor.mottag@gmail.com>
2023-01-25 20:30:55 -0300

    Fix: Merge

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,45 +1,55 @@
 import { usersCollection } from "../config/databases.js";
-import bcryt from "bcrypt";
+import bcrypt from "bcrypt";
 
 export async function signUp(req, res) {
-    const { name, avatar, email, confirmEmail, password, confirmPassword } = req.body;
+  const { name, avatar, email, confirmEmail, password, confirmPassword } = req.body;
 
-    try {
-        const userAlreadyExists = await usersCollection.findOne({
-            $or: [({ email }, { name })],
-        });
-
-        if (userAlreadyExists) {
-            return res.status(409).send("Username or email already exists");
-        }
-    } catch (error) {
-        console.log(`signUp: findOne error for name:${name} with email:${email} !`, error.message);
-        res.status(500).send(error.message);
-    }
-
-    const encryptedPassword = await bcryt.hash(password, 10).catch((err) => {
-        console.log(`"signUp: bcrypt.hash error for  password:"`, password, err.message);
+  try {
+    const userAlreadyExists = await usersCollection.findOne({
+      $or: [({ email }, { name })],
     });
 
-    const now = new Date();
-
-    const user = await usersCollection
-        .insertOne({
-            name,
-            avatar,
-            email,
-            password: encryptedPassword,
-            createdAt: now,
-            updatedAt: now,
-        })
-        .catch((err) => {
-            console.log("signUp: insertOne error for user:", user, err.message);
-        });
-    if (!user) {
-        return res.status(500).send("Error creating user");
+    if (userAlreadyExists) {
+      return res.status(409).send("Username or email already exists");
     }
+  } catch (error) {
+    console.log(`signUp: findOne error for name:${name} with email:${email} !`, error.message);
+    res.status(500).send(error.message);
+  }
 
-    return res.status(201).send("User created");
+  const encryptedPassword = await bcrypt.hash(password, 10).catch((err) => {
+    console.log(`"signUp: bcrypt.hash error for  password:"`, password, err.message);
+  });
+
+  const now = new Date();
+
+  const user = await usersCollection
+    .insertOne({
+      name,
+      avatar,
+      email,
+      password: encryptedPassword,
+      createdAt: now,
+      updatedAt: now,
+    })
+    .catch((err) => {
+      console.log("signUp: insertOne error for user:", user, err.message);
+    });
+  if (!user) {
+    return res.status(500).send("Error creating user");
+  }
+
+  return res.status(201).send("User created");
 }
 
-export async function signIn(req, res) {}
+export async function signIn(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const checkUser = await usersCollection.findOne({ email });
+
+    if (!checkUser) {
+      return res.status(401).send("E-mail ou senha incorretos!");
+    }
+  } catch (error) {}
+}
