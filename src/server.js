2081a7272683b5943d7576773d99f34da98e1370
Victor Motta <victor.mottag@gmail.com>
2023-01-25 18:36:38 -0300

    Adicionando dot env

--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = process.env.PORT || 5000;
-
-
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 //mamao doce
 
-server.listen(
-  port,
-  console.log(`Servidor iniciado com sucesso! Na porta: ${port}`)
-);
+server.listen(port, console.log(`Servidor iniciado com sucesso! Na porta: ${port}`));
