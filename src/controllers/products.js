46f469291e29b0375f9a8f26b73de91ee0da5755
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-25 14:45:31 -0300

    feat: adiciona um limit e um offset na solicitação get* dos products

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,8 +1,17 @@
 import { productsCollection } from "../config/databases.js";
 
 export async function products(req, res) {
+  const { limit, offset } = req.query;
+  if (!limit || !offset) {
+    limit = 0;
+    skip = 0;
+  }
   try {
-    const products = productsCollection.find({});
+    const products = await productsCollection
+      .find({})
+      .skip(offset)
+      .limit(limit)
+      .toArray();
     res.status(200).send(products);
   } catch (error) {
     res.status(500).send(error.message);
