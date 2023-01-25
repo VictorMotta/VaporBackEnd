bed0c371e6cc6953e1f8d58947d0b7fdff8512ee
Victor Motta <victor.mottag@gmail.com>
2023-01-25 19:14:41 -0300

    Feat: implementado inicio sign-in

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,50 +2,44 @@ import { usersCollection } from "../config/databases.js";
 import bcryt from "bcrypt";
 
 export async function signUp(req, res) {
-  const { name, avatar, email, confirmEmail, password, confirmPassword } =
-    req.body;
+    const { name, avatar, email, confirmEmail, password, confirmPassword } = req.body;
 
-  try {
-    const userAlreadyExists = await usersCollection.findOne({
-      $or: [({ email }, { name })],
-    });
+    try {
+        const userAlreadyExists = await usersCollection.findOne({
+            $or: [({ email }, { name })],
+        });
 
-    if (userAlreadyExists) {
-      return res.status(409).send("Username or email already exists");
+        if (userAlreadyExists) {
+            return res.status(409).send("Username or email already exists");
+        }
+    } catch (error) {
+        console.log(`signUp: findOne error for name:${name} with email:${email} !`, error.message);
+        res.status(500).send(error.message);
     }
-  } catch (error) {
-    console.log(
-      `signUp: findOne error for name:${name} with email:${email} !`,
-      error.message
-    );
-    res.status(500).send(error.message);
-  }
 
-  const encryptedPassword = await bcryt.hash(password, 10).catch((err) => {
-    console.log(
-      `"signUp: bcrypt.hash error for  password:"`,
-      password,
-      err.message
-    );
-  });
-
-  const now = new Date();
-
-  const user = await usersCollection
-    .insertOne({
-      name,
-      avatar,
-      email,
-      password: encryptedPassword,
-      createdAt: now,
-      updatedAt: now,
-    })
-    .catch((err) => {
-      console.log("signUp: insertOne error for user:", user, err.message);
+    const encryptedPassword = await bcryt.hash(password, 10).catch((err) => {
+        console.log(`"signUp: bcrypt.hash error for  password:"`, password, err.message);
     });
-  if (!user) {
-    return res.status(500).send("Error creating user");
-  }
 
-  return res.status(201).send("User created");
+    const now = new Date();
+
+    const user = await usersCollection
+        .insertOne({
+            name,
+            avatar,
+            email,
+            password: encryptedPassword,
+            createdAt: now,
+            updatedAt: now,
+        })
+        .catch((err) => {
+            console.log("signUp: insertOne error for user:", user, err.message);
+        });
+    if (!user) {
+        return res.status(500).send("Error creating user");
+    }
+
+    return res.status(201).send("User created");
 }
+
+export async function signIn(req, res) {}
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.js";
+import { signIn, signUp } from "../controllers/auth.js";
 import validateSchema from "../middlewares/schemaValidation.js";
 import { signUpSchema } from "../schemas/authSchemas.js";
 
 const AuthRouter = Router();
 
 AuthRouter.post("/sign-up", validateSchema(signUpSchema), signUp);
+AuthRouter.post("/sign-in", signIn);
 
 export default AuthRouter;
